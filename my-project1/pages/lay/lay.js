d8c6c8bb5cbb4297a744aad3e14a17bbbc76b774
traffic <956569776@qq.com>
2018-08-17 09:39:38 +0800

    first commit

---
+++ my-project1/pages/lay/lay.js
@@ -0,0 +1,13 @@
+var helloData = {
+  name: 'wechat'
+}
+
+Page({
+  data: helloData,
+  changeName: function(e) {
+    // sent data change to view
+    this.setData({
+      name: 'MINA'
+    })
+  }
+})
